00157b568a7a6a6dc30b86b19f8c687febc9218f
Javier de Martín Gil <javierdemartin@me.com>
2019-08-07 07:43:35 +0200

    Latest update

--- transform/render.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(currentFile, filesInCurrentFolder, allFiles) {
-  return Promise.resolve(
-    '<code>'
-      +JSON.stringify(currentFile, null, 2)+' of '+allFiles.length
-      +'\n'
-      +JSON.stringify(filesInCurrentFolder, null, 2)
-    +'</code>'
-  );
-};
